cdff3d5112a1cf91f487571f9312a200e82fb16f
sumchive <haon0319@naver.com>
2025-10-14 14:15:25 +0900

    [setting]: 초기 프로젝트 세팅

---
+++ sopt-ios-seminar01/sopt-ios-seminar01/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 10/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 15)
+        label.clipsToBounds = true
+        return label
+    }()
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+    
+    private func setUI() {
+        view.addSubview(testLabel)
+        view.backgroundColor = .yellow
+    }
+    
+    private func setLayout() {
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -150)
+        ])
+    }
+}
+
