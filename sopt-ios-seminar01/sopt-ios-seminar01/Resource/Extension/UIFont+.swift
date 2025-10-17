d214efebd63bc2ae66ab10bcbb02a8487fb63f99
sumchive <haon0319@naver.com>
2025-10-17 10:29:56 +0900

    [add]: Pretendard 폰트 추가

---
+++ sopt-ios-seminar01/sopt-ios-seminar01/Resource/Extension/UIFont+.swift
@@ -0,0 +1,27 @@
+//
+//  UIFont+.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 10/17/25.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static var head1: UIFont = UIFont(name: "Pretendard-ExtraBold", size: 30)!
+    
+    
+    static var subhead1: UIFont = UIFont(name: "Pretendard-Bold", size: 18)!
+    
+    
+    static var subhead2: UIFont =  UIFont(name: "Pretendard-Regular", size: 16)!
+    
+    static var subhead3: UIFont =  UIFont(name: "Pretendard-Bold", size: 15)!
+    
+    static var subhead4: UIFont =  UIFont(name: "Pretendard-SemiBold", size: 14)!
+    
+    static var subhead5: UIFont = UIFont(name: "Pretendard-Regular", size: 14)!
+    
+    static var subhead6: UIFont = UIFont(name: "Pretendard-Regular", size: 12)!
+}
