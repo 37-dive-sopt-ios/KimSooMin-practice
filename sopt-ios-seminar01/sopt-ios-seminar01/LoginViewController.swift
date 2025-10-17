926740b99f46513403694d5d4f37e293669cb5f4
sumchive <haon0319@naver.com>
2025-10-17 13:58:56 +0900

    [feat]: 슬로건 문구

---
+++ sopt-ios-seminar01/sopt-ios-seminar01/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 10/17/25.
+//
+
+import Foundation
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    private let sloganLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 47, y: 163, width: 307, height: 70))
+        label.text = "동네라서 가능한 모든 것\n당근에서 가까운 이웃과 함께해요."
+        label.textAlignment = .center
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.font = .subhead1
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setLayout()
+    }
+    
+    private func setLayout() {
+        [sloganLabel]
+            .forEach {
+            self.view.addSubview($0)
+        }
+    }
+}
---
+++ sopt-ios-seminar01/sopt-ios-seminar01/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 10/17/25.
+//
+
+import Foundation
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
