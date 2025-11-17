02fbf3753fc4d2de867cfe7f66ac840f0be54a8e
sumchive <haon0319@naver.com>
2025-11-17 10:19:14 +0900

    [week4] 네트워크 통신 실습

---
+++ sopt-ios-seminar01/sopt-ios-seminar01/Week4/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 11/8/25.
+//
+
+import Foundation
+
+enum Environment {
+  static let baseURL: String = "http://15.164.129.239"
+}
